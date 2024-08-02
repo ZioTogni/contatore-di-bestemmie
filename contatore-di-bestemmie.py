1b1e73357b36ff33c4d99ea771fd702695b7f856
Manuel <159720066+ZioTogni@users.noreply.github.com>
2024-08-02 14:48:05 +0200

    Add files via upload

---
+++ contatore-di-bestemmie.py
@@ -0,0 +1,62 @@
+#VARIABILI
+risposta_1="Ho fatto"
+bestemmie="Porco Dio"
+risposta_riavvio="Sì"
+risposta_chiusura="No"
+#PROGRAMMA
+print("Benvenuto nel contatore di bestemmie, leggi il file ReadMe per le istruzioni!")
+print("\nAppena hai finito di leggere il file scrivi <<Ho fatto>>")
+risposta_utente=input()
+if risposta_utente==risposta_1:
+    print("Bene, divertiti con le bestemmie, esagera pure!")
+else:
+    print("Il programma parte solo se scrivi <<ho fatto>>, riavvia il programma e leggi attentamente le istruzioni!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("1")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("2")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("3")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("4")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("5")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("6")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("7")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("8")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("9")
+else:
+    print("Abbiamo qualche errore qui, riavvia il programma perfavore!")
+bestemmie_utente=input()
+if bestemmie_utente==bestemmie:
+    print("10")
+    print("\nSei arrivato a 10 bestemmie, congratulazioni!")
